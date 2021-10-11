1131ad4045a90b69ddaeb745d76173548a69f1aa
Thibault Valton <t.valton@gmail.com>
2021-10-11 15:07:52 +0200

    Front wip

--- src/main/java/com/example/hotel/HotelApplication.java
+++ src/main/java/com/example/hotel/HotelApplication.java
@@ -2,6 +2,8 @@ package com.example.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HotelApplication {
@@ -9,5 +11,15 @@ public class HotelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
 	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("").allowedMethods("*");
+			}
+		};
+	}
+
 
 }
